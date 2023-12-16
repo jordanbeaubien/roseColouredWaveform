bbf3ccf7afb3442c2b5c0000d53f493a2e836261
jordanbeaubien <jordanbeaubien@me.com>
2023-12-15 19:58:16 -0700

    add necessary imports

---
+++ __RCW__/transformation.py
@@ -0,0 +1,7 @@
+# logic from prototype goes here
+
+import librosa              # pip install librosa
+import numpy as np          # pip install numpy
+from scipy import signal    # pip install scipy
+import soundfile            # pip install soundfile
+
