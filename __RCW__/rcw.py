81aff29c16f82e0ba8219d3fb90632bee93127e4
jordanbeaubien <jordanbeaubien@me.com>
2023-12-15 19:57:19 -0700

    display blank interface

---
+++ __RCW__/rcw.py
@@ -0,0 +1,27 @@
+
+
+# import functions for terminal user interface
+from userfacing import rcw_interface
+
+import transformation   # import functions sound transforming
+
+
+interface = rcw_interface()
+interface.displayInterface()
+
+# get input filename
+interface.getInputFile()
+interface.displayInterface()
+
+# user default settings?
+# interface.setDefault()
+# interface.displayInterface()
+
+
+# for each setting, if(self.___) then don't set it
+
+# if all are not unknown (all are known) then ask to submit.
+
+
+if __name__ == "__main__":
+  pass
